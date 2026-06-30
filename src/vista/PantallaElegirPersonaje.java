27c2654cef6212073108d99616a45b70b5c3a36a
rociofis <rfischer@uade.edu.ar>
2026-06-30 20:59:39 -0300

    Arreglos imagenes

--- src/vista/PantallaElegirPersonaje.java
+++ src/vista/PantallaElegirPersonaje.java
@@ -28,11 +28,9 @@ public class PantallaElegirPersonaje extends JDialog {
     private static final Color COLOR_TEXTO  = Color.WHITE;
 
     public PantallaElegirPersonaje(JFrame parent) {
-<<<<<<< Updated upstream
+
         super(parent, "Elegir Personaje", true);
-=======
-        super(parent, "Crear Personaje", true);
->>>>>>> Stashed changes
+
         setUndecorated(true);
         initUI();
 
